d561fa1d13d64596527188f8e10dbbd825cf7437
vino <vino0908@outlook.com>
2021-09-27 11:32:34 +0800

    npx prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import {resolve} from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src')
-        }
-    },
-    base: './',
-    server: {
-        port: 3324,
-        open: true,
-        cors: true
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
+  },
+  base: './',
+  server: {
+    port: 3324,
+    open: true,
+    cors: true
+  }
 })
-;
